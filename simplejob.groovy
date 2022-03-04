08c61c4ed207f2fe196a03eed4d269a111825ade
Cheenath, Jackson <Jackson.Cheenath@ge.com>
2022-03-04 16:51:06 -0600

    Test pipeline job

--- simplejob.groovy
+++ simplejob.groovy
@@ -1,5 +1,44 @@
-job('example') {
-  steps {
-    batchFile('echo Hello World!')
-  }
+@Library('mylibrary') _
+
+import com.jcheenatcode.GlobalVars
+
+pipelineJob('example') {
+    properties {
+        buildDiscarder {
+            strategy {
+                logRotator {
+                    daysToKeepStr('60')
+                    numToKeepStr('60')
+                    artifactDaysToKeepStr('-1')
+                    artifactNumToKeepStr('-1')
+                }
+            }
+        }
+        githubProjectProperty {
+            projectUrlStr('')
+            displayName('')
+        }
+
+        parametersDefinitionProperty {
+            parameterDefinitions {
+                stringParameterDefinition {
+                    name('module_name')
+                    description('module name description')
+                    defaultValue('Test')
+                    trim(false)
+                }
+            }
+        }
+    }
 }
+
+// job('example') {
+//   steps {
+//     batchFile('echo Hello World!')
+
+//     echo 'Hello, world'
+//     sayHello 'Dave'
+
+//     echo 'The value of foo is : ' + GlobalVars.foo
+//   }
+// }
