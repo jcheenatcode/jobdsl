31a7eddfad1f87f800e331263b6a3bd03546f648
Cheenath, Jackson <Jackson.Cheenath@ge.com>
2022-03-05 02:07:51 -0600

    readyaml

--- multiJobWithJobDSL.groovy
+++ multiJobWithJobDSL.groovy
@@ -1,8 +1,8 @@
-@Library('mylibrary') _
-
-import com.jcheenatcode.GlobalVars
+datas = readYaml(file:'dev.yaml')
+echo datas.toString()
 
 String[] jobs = ["ex_1", "ex_2", "ex_3"]
+
 
 jobs.each { item, idx ->
     job("example_${item}") {
