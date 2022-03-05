893b1f82fb2cccce638c19e020fd9be5b6503fb3
Cheenath, Jackson <Jackson.Cheenath@ge.com>
2022-03-05 16:54:26 -0600

    organizing

--- src/com/jcheenatcode/jobTemplates/multiJobWithJobDSL.groovy
+++ src/com/jcheenatcode/jobTemplates/multiJobWithJobDSL.groovy
@@ -1,21 +1,18 @@
 @Grab(group='org.codehaus.groovy', module='groovy-yaml', version='3.0.9')
 import groovy.yaml.YamlSlurper
 
-def yamlFile = readFileFromWorkspace('jobs.yaml')
+def yamlFile = readFileFromWorkspace('config/jobs.yaml')
 println yamlFile
 
-// Create YAML file.
-// def yamlFile = new File("./jobs.yaml")
 def datas;
 
 datas = new YamlSlurper().parseText(yamlFile)
 println datas.toString()
 
-String[] jobs = ["ex_1", "ex_2", "ex_3"]
-
+def jobs = datas.jobs;
 
 jobs.eachWithIndex { item, idx ->
-    job("example_${item}") {
+    job("example_${item.project}") {
         steps {
             batchFile('echo Hello World!')
         }
